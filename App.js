43ddb517366ee69a12a916b79f6eda31a2d0c47c
TeekmaRam <teekmchoudhary007@gmail.com>
2021-10-29 01:55:37 +0530

    my first commit

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
-import { View, Text} from 'react-native';
+import { View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import { Provider } from 'react-redux'
-import CountryName from './Mainpage/Countryname'
-import Store from './ReduxReducer/Stores'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DrawerHome from './Api/NewDrawerscreen'
+import Exitscreen from './Api/ExitScreen'
+import Layoutpage from './Api/Layoutpage'
+import Countryshareborder from './Api/Contryshareborder'
 
 
 const App = () => {
@@ -12,11 +16,40 @@ const App = () => {
   SplashScreen.hide();
  }, [])
 
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+  
+const Appdrawer = () => {
+  return(
+  <Drawer.Navigator>
+<Drawer.Screen name = 'Home' component={DrawerHome} />
+<Drawer.Screen name = 'Exit' component={Exitscreen} />
+  </Drawer.Navigator>
+  )
+}
+
+
+
+
+const StackNavigation = () => {
+  return(
+    <Stack.Navigator initialRouteName="Home" headerMode='none'>
+      <Stack.Screen name='Appdrawer' component={Appdrawer} />
+      <Stack.Screen name='Layoutpage' component={Layoutpage} />
+      <Stack.Screen name='Countryshareborder' component={Countryshareborder} />
+    </Stack.Navigator>
+  )
+}
 
   return (
- <Provider store={Store}>
-      <CountryName />
-</Provider> 
+    <NavigationContainer>
+      <StackNavigation />
+    </NavigationContainer>
      
-  )}
+  );
+  
+}
+
+
+
 export default App;
--- ReduxReducer/Action-type.js
+++
@@ -1,4 +0,0 @@
-export const Fetch_Request = 'Fetch_Request'
-export const Fetch_Sucess = 'Fetch_Sucess'
-export const Fetch_Falure = 'Fetch_Falure'
-
